b1ba2a33a8c82ab2093cf6a8315ff1b0a1667ea2
gahmad <ghufran_ahmad@mentor.com>
2017-10-28 19:13:29 +0500

    Adding Board and BoardState classes

---
+++ src/chess/Board.java
@@ -0,0 +1,209 @@
+package chess;
+
+import pieces.Bishop;
+import pieces.King;
+import pieces.Knight;
+import pieces.Pawn;
+import pieces.Piece;
+import pieces.Queen;
+import pieces.Rook;
+
+public class Board {
+	private Cell board[][];
+	public static final int ROWS = 8;
+	public static final int COLUMNS = 8;
+
+	private Rook whiteRook01, whiteRook02, blackRook01, blackRook02;
+	private Knight whiteKinght01, whiteKnight02, blackKnight01, blackKnight02;
+	private Bishop whiteBishop01, whiteBishop02, blackBishop01, blackBishop02;
+	private Pawn whitePawn[], blackPawn[];
+	private Queen whiteQueen, blackQueen;
+	private King whiteKing, blackKing;
+
+	public Board() {
+
+		initializePieces();
+
+		initializeBoard();
+	}
+
+	private void initializeBoard() {
+		board = new Cell[ROWS][COLUMNS];
+
+		for (int i = 0; i < ROWS; i++)
+			for (int j = 0; j < COLUMNS; j++) {
+
+				Piece piece = null;
+				if (i == 0 && j == 0)
+					piece = blackRook01;
+				else if (i == 0 && j == 7)
+					piece = blackRook02;
+				else if (i == 7 && j == 0)
+					piece = whiteRook01;
+				else if (i == 7 && j == 7)
+					piece = whiteRook02;
+				else if (i == 0 && j == 1)
+					piece = blackKnight01;
+				else if (i == 0 && j == 6)
+					piece = blackKnight02;
+				else if (i == 7 && j == 1)
+					piece = whiteKinght01;
+				else if (i == 7 && j == 6)
+					piece = whiteKnight02;
+				else if (i == 0 && j == 2)
+					piece = blackBishop01;
+				else if (i == 0 && j == 5)
+					piece = blackBishop02;
+				else if (i == 7 && j == 2)
+					piece = whiteBishop01;
+				else if (i == 7 && j == 5)
+					piece = whiteBishop02;
+				else if (i == 0 && j == 3)
+					piece = blackKing;
+				else if (i == 0 && j == 4)
+					piece = blackQueen;
+				else if (i == 7 && j == 3)
+					piece = whiteKing;
+				else if (i == 7 && j == 4)
+					piece = whiteQueen;
+				else if (i == 1)
+					piece = blackPawn[j];
+				else if (i == 6)
+					piece = whitePawn[j];
+
+				Cell cell = new Cell(i, j, piece);
+				board[i][j] = cell;
+			}
+	}
+
+	public Board(Board oldBoard) {
+		initializePieces();
+		initializeBoard();
+		
+		for (int i = 0; i < Board.ROWS; i++)
+			for (int j = 0; j < Board.COLUMNS; j++) {
+				try {
+					board[i][j] = new Cell(oldBoard.getCell(i, j));
+				} catch (CloneNotSupportedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		copyPieces(oldBoard);
+	}
+
+	private void copyPieces(Board copyFrom) {
+		whiteRook01 = copyFrom.whiteRook01;
+		whiteRook02 = copyFrom.whiteRook02;
+		blackRook01 = copyFrom.blackRook01;
+		blackRook02 = copyFrom.blackRook02;
+		whiteKinght01 = copyFrom.whiteKinght01;
+		whiteKnight02 = copyFrom.whiteKnight02;
+		blackKnight01 = copyFrom.blackKnight01;
+		blackKnight02 = copyFrom.blackKnight02;
+		whiteBishop01 = copyFrom.whiteBishop01;
+		whiteBishop02 = copyFrom.whiteBishop02;
+		blackBishop01 = copyFrom.blackBishop01;
+		blackBishop02 = copyFrom.blackBishop02;
+		whiteQueen = copyFrom.whiteQueen;
+		blackQueen = copyFrom.blackQueen;
+		whiteKing = copyFrom.whiteKing;
+		blackKing = copyFrom.blackKing;
+		whitePawn = copyFrom.whitePawn;
+		blackPawn = copyFrom.blackPawn;
+	}
+
+	public Cell[][] getBoard() {
+		return board;
+	}
+
+	private void initializePieces() {
+		initializeRooks();
+
+		initializeKnights();
+
+		initializeBishops();
+
+		initializeQueens();
+
+		initializeKings();
+
+		initializePawns();
+	}
+
+	private void initializePawns() {
+		whitePawn = new Pawn[8];
+		blackPawn = new Pawn[8];
+		for (int i = 0; i < 8; i++) {
+			whitePawn[i] = new Pawn("WP0" + (i + 1), "White_Pawn.png", 0);
+			blackPawn[i] = new Pawn("BP0" + (i + 1), "Black_Pawn.png", 1);
+		}
+	}
+
+	private void initializeKings() {
+		whiteKing = new King("WK", "White_King.png", 0, 7, 3);
+		blackKing = new King("BK", "Black_King.png", 1, 0, 3);
+	}
+
+	private void initializeQueens() {
+		whiteQueen = new Queen("WQ", "White_Queen.png", 0);
+		blackQueen = new Queen("BQ", "Black_Queen.png", 1);
+	}
+
+	private void initializeBishops() {
+		whiteBishop01 = new Bishop("WB01", "White_Bishop.png", 0);
+		whiteBishop02 = new Bishop("WB02", "White_Bishop.png", 0);
+		blackBishop01 = new Bishop("BB01", "Black_Bishop.png", 1);
+		blackBishop02 = new Bishop("BB02", "Black_Bishop.png", 1);
+	}
+
+	private void initializeKnights() {
+		whiteKinght01 = new Knight("WK01", "White_Knight.png", 0);
+		whiteKnight02 = new Knight("WK02", "White_Knight.png", 0);
+		blackKnight01 = new Knight("BK01", "Black_Knight.png", 1);
+		blackKnight02 = new Knight("BK02", "Black_Knight.png", 1);
+	}
+
+	private void initializeRooks() {
+		whiteRook01 = new Rook("WR01", "White_Rook.png", 0);
+		whiteRook02 = new Rook("WR02", "White_Rook.png", 0);
+		blackRook01 = new Rook("BR01", "Black_Rook.png", 1);
+		blackRook02 = new Rook("BR02", "Black_Rook.png", 1);
+	}
+
+	public King getBlackKing() {
+		return blackKing;
+	}
+
+	public King getWhiteKing() {
+		return whiteKing;
+
+	}
+
+	public void updateWhiteKingLocation(int x, int y) {
+		whiteKing.setx(x);
+		whiteKing.sety(y);
+
+	}
+
+	public void updateBlackKingLocation(int x, int y) {
+		blackKing.setx(x);
+		blackKing.sety(y);
+
+	}
+
+	public Cell getCell(int xIndex, int yIndex) {
+		if (xIndex < 0 || xIndex >= ROWS || yIndex < 0 || yIndex >= COLUMNS)
+			return null;
+
+		return board[xIndex][yIndex];
+	}
+
+	public Piece getPiece(int xIndex, int yIndex) {
+		Cell cell = getCell(xIndex, yIndex);
+		if (cell == null)
+			return null;
+
+		return cell.getpiece();
+	}
+}
---
+++ src/chess/BoardState.java
@@ -0,0 +1,117 @@
+package chess;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+import pieces.King;
+import pieces.Piece;
+
+public class BoardState implements MouseListener {
+	private Board chessBoard;
+
+	private Main mainClassRef;
+
+	public BoardState(JPanel boardPanel, Main ref) {
+		mainClassRef = ref;
+		chessBoard = new Board();
+
+		for (int i = 0; i < Board.ROWS; i++)
+			for (int j = 0; j < Board.COLUMNS; j++) {
+				Cell cell = chessBoard.getCell(i, j);
+				if (cell == null) {
+					System.out.println("Cell: " + i + " - " + j + " is NULL");
+				}
+
+				cell.addMouseListener(this);
+				boardPanel.add(cell);
+			}
+	}
+
+	private BoardState(BoardState oldBoardState) {
+		chessBoard = new Board(oldBoardState.chessBoard);
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		BoardState newBoardState = new BoardState(this);
+		return newBoardState;
+	}
+
+	public King getKing(int color) {
+		if (color == 0)
+			return chessBoard.getWhiteKing();
+		else
+			return chessBoard.getBlackKing();
+	}
+
+	public void updateKing(int color, int x, int y) {
+		if (color == 0)
+			chessBoard.updateWhiteKingLocation(x, y);
+		else
+			chessBoard.updateBlackKingLocation(x, y);
+	}
+	
+	public void updateKing(int color, Cell newCell) {
+		if (color == 0)
+			chessBoard.updateWhiteKingLocation(newCell.x, newCell.y);
+		else
+			chessBoard.updateBlackKingLocation(newCell.x, newCell.y);
+	}
+	
+	
+	public void updateKing(King piece, Cell newCell) {
+		piece.setx(newCell.x);
+		piece.sety(newCell.y);
+	}
+	
+	public Cell[][] getChessBoard() {
+		return chessBoard.getBoard();
+	}
+	
+	public void setChessBoard(int i, int j, Piece piece) {
+		chessBoard.getCell(i, j).setPiece(piece);
+	}
+	
+	public Cell getCell(King piece) {
+		return chessBoard.getCell(piece.getx(), piece.gety());
+	}
+
+	public Cell getCell(Cell cell) {
+		return chessBoard.getCell(cell.x, cell.y);
+	}
+
+	public Cell getCell(int i, int j) {
+		return chessBoard.getCell(i, j);
+	}
+	
+	public Piece getPiece(int i, int j) {
+		return chessBoard.getPiece(i, j);
+	}
+	public Piece getPiece(Cell cell) {
+		return chessBoard.getPiece(cell.x, cell.y);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		mainClassRef.makeMove((Cell) arg0.getSource());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+	}
+
+}
